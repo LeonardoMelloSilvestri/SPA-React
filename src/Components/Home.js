5102795e25ea43ea8045c47a5cfefbaf7933e8c3
Leonardo Silvestri <leonardomscontato@gmail.com>
2019-11-22 13:25:06 -0200

    Using Axios and REST API JSONPlaceholder to fill some Bootstrap elements with data for me

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,56 +1,51 @@
 import React from 'react';
+import axios from 'axios';
 
-const Home = () => {
-  return (
-    <div className="home">
-      <div className="jumbotron">
+class Home extends React.Component {
+  state = {
+    infos: []
+  }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+        this.setState({
+          infos: res.data.slice(0, 4)
+        })
+      })
+  }
+
+  render() {
+    const infoList = this.state.infos.map(info => {
+      return(
+        <div className="col-sm-6 col-lg-3">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">{info.title}</h5>
+              <p className="card-text">{info.body}</p>
+              <a href="#" className="btn btn-primary">Check it out!</a>
+            </div>
+          </div>
+        </div>
+      )
+    })
+
+    return (
+      <div className="home">
+        <div className="jumbotron">
+          <div className="container">
+            <h1 className="display-4">Fluid jumbotron</h1>
+            <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+          </div>
+        </div>
         <div className="container">
-          <h1 className="display-4">Fluid jumbotron</h1>
-          <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-        </div>
-      </div>
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-6 col-lg-3">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-6 col-lg-3 mt-3 mt-sm-0">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-6 col-lg-3 mt-3 mt-lg-0">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-6 col-lg-3 mt-3 mt-lg-0">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
+          <div className="row">
+            {infoList}
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Home;

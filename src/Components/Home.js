b55360eca3a4b856c41746404b2fae4ab0131923
Leonardo Silvestri <leonardomscontato@gmail.com>
2019-11-23 03:02:43 -0200

    Turning Home into a class component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 
-class Home extends React.Component {
+class Home extends Component {
   state = {
     infos: []
   }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
         <Link to="/" className="navbar-brand">FF12Fan</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -16,10 +16,22 @@ const Navbar = () => {
               <Link to='/' className="nav-link">Home</Link>
             </li>
             <li className="nav-item">
-            <NavLink to='/enredo' className="nav-link">Enredo</NavLink>
+              <NavLink to='/enredo' className="nav-link">Enredo</NavLink>
             </li>
-            <li className="nav-item">
-            <NavLink to='/personagens' className="nav-link">Personagens</NavLink>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Personagens
+              </a>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <Link to='/personagem/Vaan' className="dropdown-item">Vaan</Link>
+                <Link to='/personagem/Penelo' className="dropdown-item">Penelo</Link>
+                <Link to='/personagem/Balthier' className="dropdown-item">Balthier</Link>
+                <Link to='/personagem/Fran' className="dropdown-item">Fran</Link>
+                <Link to='/personagem/Basch' className="dropdown-item">Basch</Link>
+                <Link to='/personagem/Ashe' className="dropdown-item">Ashe</Link>
+                <div className="dropdown-divider"></div>
+                <Link to="/personagens" className="dropdown-item">NPCs importantes</Link>                
+              </div>
             </li>
           </ul>
         </div>

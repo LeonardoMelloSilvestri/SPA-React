602c5176630442375f28cdc42f1a0b5f6f34a663
Leonardo Silvestri <leonardomscontato@gmail.com>
2019-11-23 03:07:04 -0200

    Turning characters array into npc array and other minor changes

--- src/Components/Personagens.js
+++ src/Components/Personagens.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import axios from 'axios';
+import React, { Component } from 'react'
+import axios from 'axios'
 
-export default class Personagens extends React.Component {
+export default class Personagens extends Component {
   state = {
-    characters: []
+    npcs: []
   }
 
   componentDidMount() {
     axios.get('https://jsonplaceholder.typicode.com/photos')
       .then(res => {
         this.setState({
-          characters: res.data.slice(0, 6)
+          npcs: res.data.slice(0, 6)
         })
       })
   }
 
   render() {
-    const charactersList = this.state.characters.map(character => {
+    const npcsList = this.state.npcs.map(npc => {
       return (
-        <div className="col-lg-4 text-center" key={character.id}>
-          <img className="rounded-circle mb-3" width="140" height="140" src={character.url}></img>
+        <div className="col-lg-4 text-center" key={npc.id}>
+          <img className="rounded-circle mb-3" width="140" height="140" src={npc.url}></img>
           <h2>Heading</h2>
           <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-          <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
         </div>
       )
     })
@@ -30,7 +29,7 @@ export default class Personagens extends React.Component {
       <div className="personagens">
         <div className="container mt-3">
           <div className="row">
-            {charactersList}
+            {npcsList}
           </div>
         </div>
       </div>
